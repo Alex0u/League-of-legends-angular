16cffbd7813f8358eb268038e141f190de7de46b
Alex Romain <alexdan.romain@gmail.com>
2022-05-02 12:38:22 +0200

    JSON now imported thanks to json-typings.d.ts & initializing in-memory-web-api with dataset

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { IChampion } from '../utils/interface';
+import firstChampionDataset from '../../assets/dataset/champion_info.json';
+import secondChampionDataset from '../../assets/dataset/champion_info_2.json';
+import spellDataset from '../../assets/dataset/summoner_spell_info.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-
   constructor() { }
+
+  createDb() {
+    const champions: IChampion[] = Object.values(firstChampionDataset.data);
+    const champions2: IChampion[] = Object.values(secondChampionDataset.data);
+    const spells: IChampion[] = Object.values(spellDataset.data);
+
+    return { champions, champions2, spells };
+  }
 }
