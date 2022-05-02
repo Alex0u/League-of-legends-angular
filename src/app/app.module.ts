928e8d4d8d665a2584015af200a677798764b86f
Alex Romain <alexdan.romain@gmail.com>
2022-05-02 13:31:00 +0200

    Addition of an appbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Material Modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
 // Components
 import { HomeComponent } from './components/home/home.component';
 import { AppComponent } from './app.component';
 
 // HTTP
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import {HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 // Services
 import { DataService } from './services/data.service';
+import { AppBarComponent } from './components/app-bar/app-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AppBarComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +34,10 @@ import { DataService } from './services/data.service';
     // Avoid creating multiple instances of ChampionService by passing it into forRoot
     HttpClientInMemoryWebApiModule.forRoot(DataService, { dataEncapsulation: false }),
     HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
