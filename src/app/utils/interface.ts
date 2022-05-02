c7a1aecb387f05655322cdff44de2670b7f125f4
Alex Romain <alexdan.romain@gmail.com>
2022-05-02 12:24:54 +0200

    Addition of spell interface and champion interface

---
+++ src/app/utils/interface.ts
@@ -0,0 +1,74 @@
+export interface IChampion {
+  /**
+   * @description The champion's ID.
+   * 
+   * @type {number}
+   */
+  id: number;
+
+  /**
+   * @description The champion's title.
+   * 
+   * @type {string}
+   */
+  title: string;
+
+  /**
+   * @description The champion's name.
+   * 
+   * @type {string}
+   */
+  name: string;
+
+  /**
+   * @description The champion's key.
+   * 
+   * @type {string}
+   */
+  key: string;
+
+  /**
+   * @description The champion's list of tag.
+   * 
+   * @type {string[]}
+   */
+  tags: string[];
+}
+
+export interface ISummonerSpell {
+  /**
+   * @description The spell's ID.
+   * 
+   * @type {number}
+   */
+  id: number;
+
+  /**
+   * @description The summoner level.
+   * 
+   * @type {number}
+   */
+  summonerLevel: number;
+
+  /**
+   * @description The spell's name.
+   * 
+   * @type {string}
+   */
+  name: string;
+
+  /**
+   * @description The spell's key.
+   * 
+   * @type {string}
+   */
+  key: string;
+
+
+  /**
+   * @description The spell's description.
+   * 
+   * @type {string}
+   */
+  description: string;
+}
